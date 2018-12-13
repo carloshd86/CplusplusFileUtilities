054785d6abe4f5172d990984657bb9c6b0e6dd67
carloshdelgado <carloshdelgado@outlook.com>
2018-12-13 12:29:04 +0100

    Full work commit

---
+++ Practica6CarlosHD/IFileUtils.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "IFile.h"
+
+unsigned int CountStringAppearances(const char * strToSearch, const FileIdentifier * fileId);
+int SumCommaSeparatedIntegers(const FileIdentifier * fileId);
